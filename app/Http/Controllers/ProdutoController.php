50e0ddd079d80c49a37d44cef95fa8dfdbf17fcd
Hiago Filipe Santos de Souza <hiagofss98@gmail.com>
2019-03-05 09:26:32 -0300

    Utilizando o Blade para retirar excesso de código HTML

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -1,13 +1,24 @@
 <?php namespace estoque\Http\Controllers;
 
 use DB;
+use Request;
 
 class ProdutoController {
 
     public function lista() {
 
         $produtos = DB::select('select * from produtos');
-        
+
         return view('listagem')->with('produtos', $produtos);
+    }
+
+    public function mostra() {
+        $id = Request::route('id');
+        $produto = DB::select('SELECT * FROM produtos WHERE id = ?', [$id]);
+
+        if (empty($produto)) {
+            return "Esse produto não existe";
+        }
+        return view('detalhes')->with('p', $produto[0]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/', function()
 });
 
 Route::get('/produtos', 'ProdutoController@lista');
+
+Route::get('/produtos/mostra/{id}', 'ProdutoController@mostra');
